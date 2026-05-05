60d4603a74fbd3cb0cb477957b81846bdac2ac0a
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-05 18:09:19 +0700

    Fix main function: correct object names and ensure proper display of Mahasiswa details

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -27,3 +27,37 @@ class Mahasiswa {
         }
 };
 
+int  Mahasiswa::nim = 0;
+
+//pendefinisial prosedur - prosedur diluar class
+void  Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printALL() {
+    cout << "ID = " << id << endl;
+    cout << "NAMA = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    //pembuatan object dan pemberian nilai
+    Mahasiswa  mhs("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+    //membri nilai pada setiapNim() untuk merubah nilai  NIM
+    Mahasiswa::setNim(9);
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa  mhs4("Joko  Wahono");
+
+    //memanggil prosedur printALL()
+    mhs1.printALL();
+    mhs2.printALL();
+    mhs3.printALL();
+    mhs4.printALL();
+
+    //menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object  =  " << Mahasiswa::getNim() << endl;
+    system("pause");
+
+    return 0;
+}
