221d46f9822021cd76e0a66e74160d0feadcbdf9
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-05 18:15:11 +0700

    Add class definition for angka with constructor and destructor

--- destructor.cpp
+++ destructor.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class angka
+{
+private:
+    int *arr;
+    int pangjang;
+
+public:
+    angka(int); // constructor
+    ~angka(); // destructor
+    void cetakData();
+    void isiData();
+};
+
