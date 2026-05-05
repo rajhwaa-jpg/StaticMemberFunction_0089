56c4560667c40581221c1cbe3e94e0e812b0d80b
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-05 18:08:48 +0700

    Refactor Mahasiswa class: add static member variable and methods for nim

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -2,3 +2,28 @@
 #include <string> //menyertakan  library string
 using namespace std;
 
+class Mahasiswa {
+
+    private:
+        static int nim;
+    public:
+        int id;
+        string nama;
+
+        void setID();
+        void printALL();
+
+        //pembuatan staatic function
+        static void setNim(int pnim) {
+            nim = pnim;
+        };
+        
+        static void setNim() {
+            return nim;
+        }
+
+        Mahasiswa(string pnama) :nama(pnama) {
+            setId();
+        }
+};
+
