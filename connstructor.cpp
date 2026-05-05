9bb5a0f1a682283843db2c4f865cd4d59c45ecad
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-05 18:10:01 +0700

    Add mahasiswa class and implement constructor in connstructor.cpp

--- connstructor.cpp
+++ connstructor.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa {
+    public:
+    mahasiswa(); // constructor
+};
+
+mahasiswa::mahasiswa() {
+    cout << "Constructor Terpanggil" << endl; // Definisi connstructo
+};
+
+int  main() {
+    mahasiswa mhs; // pembuatan object
+    return 0;
+}
