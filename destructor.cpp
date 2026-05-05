a6f1a47a73f721071b4c72cf3b2c08801e027f86
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-05 18:15:25 +0700

    Fix issues in angka class: correct destructor implementation, update cetakData and isiData methods, and adjust main function

--- destructor.cpp
+++ destructor.cpp
@@ -14,3 +14,40 @@ public:
     void isiData();
 };
 
+angka::angka(int i){ // Constructor
+    pangjang = i;
+    arr = new  int[i];
+    isiData();
+}
+angka ::~angka(){ //destructor
+    cout <<  endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat ArraySudah dilepaskan" << endl;
+}
+
+void angka::cetakData()
+{
+    for  (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for  (int i = 1; 1 <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+int  mainn()
+{
+    angka brlajarcpp(3); // construoctor dipanggil
+    angka  *ptriBelajarcpp = new angka(5); //  constructor dipanggil
+    delete ptriBelajarcpp; //destructor dipanggil
+
+    return 0;
+}
