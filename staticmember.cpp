f45e526b4ac170d771420d8c5fb4a26c796ac7dd
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-05 18:06:35 +0700

    Add class definition for 'Mahasiswa' with member variables and methods

--- staticmember.cpp
+++ staticmember.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa { //membuat class mahasiswa
+    
+    public:
+        static int nim; //deklasrasi variable static yaitu variable nim
+        //deklarasi variable member
+        int id;
+        string nama;
+
+        //deklarasi prosedur setID()dan printALL()
+        void setID();
+        void printALL();
+
+        //pembuatan constructor Mahasiswa dengan parameter pnama
+        //Member initilaze list
+        Mahasiswa(string pnama) :nama(pnama){
+            setID();
+        }
+};
+
